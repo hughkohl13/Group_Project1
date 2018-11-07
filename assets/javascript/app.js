3b2b0a05a194173f35523f7809940bfabf551096
Hugh Kohl <hgkohl@us.ibm.com>
2018-11-07 11:44:50 -0800

    commented out spacers for articleBlock div in app.js file.  added articleTitle class to css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,9 +40,9 @@ function createNews(items) {
 function createNewsHeadline(node, item) {
     var row = document.createElement("div");
     row.className = "row";
-    var spacer1 = document.createElement("div");
-    spacer1.className = "articleBlock col-sm-2 col-md-2 col-lg-2";
-    row.appendChild(spacer1);
+    // var spacer1 = document.createElement("div");
+    // spacer1.className = "articleBlock col-sm-2 col-md-2 col-lg-2";
+    // row.appendChild(spacer1);
 
     var textColumns = 9;
     if (item.urlToImage) {
@@ -63,9 +63,9 @@ function createNewsHeadline(node, item) {
     createNewsContent(text, item);
     row.appendChild(text);
 
-    var spacer2 = document.createElement("div");
-    spacer2.className = "articleBlock col-sm-1 col-md-1 col-lg-1";
-    row.appendChild(spacer2);
+    // var spacer2 = document.createElement("div");
+    // spacer2.className = "articleBlock col-sm-1 col-md-1 col-lg-1";
+    // row.appendChild(spacer2);
     
     node.appendChild(row);
 }
@@ -73,17 +73,17 @@ function createNewsHeadline(node, item) {
 function createNewsRow(node, items) {
     var row = document.createElement("div");
     row.className = "row";
-    var spacer1 = document.createElement("div");
-    spacer1.className = "articleBlock col-sm-2 col-md-2 col-lg-2";
-    row.appendChild(spacer1);
+    // var spacer1 = document.createElement("div");
+    // spacer1.className = "articleBlock col-sm-2 col-md-2 col-lg-2";
+    // row.appendChild(spacer1);
 
     for (var i = 0; i < items.length; i++) {
         createNewsItem(row, items[i]);
     }
 
-    var spacer2 = document.createElement("div");
-    spacer2.className = "articleBlock col-sm-1 col-md-1 col-lg-1";
-    row.appendChild(spacer2);
+    // var spacer2 = document.createElement("div");
+    // spacer2.className = "articleBlock col-sm-1 col-md-1 col-lg-1";
+    // row.appendChild(spacer2);
     
     node.appendChild(row);
 }
