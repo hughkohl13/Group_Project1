8bfa1c83cc156b9ce0ea2bb0b2c5cbaf0db636e3
w.benarto <w.benarto@gmail.com>
2018-11-09 12:15:45 -0800

    no changes, just wanted to pull

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,5 +1,5 @@
     
-    function searchPlaceWeather(pl`````````````````````````````````````````````````````````````````````ace) {
+    function searchPlaceWeather(place) {
         var APIKey = "166a433c57516f51dfab1f7edaed8413";
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=" + APIKey;
         console.log(place);    
